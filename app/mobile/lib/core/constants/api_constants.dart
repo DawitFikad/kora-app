301909a8c91510612c5c46818f36d50726ab72a5
amanuel-alex <amanuel2123alex@gmail.com>
2025-12-26 20:40:38 +0300

    've investigated the issue and found two probable causes for the "loading for a long time" behavior in the emulator:

--- app/mobile/lib/core/constants/api_constants.dart
+++ app/mobile/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   // Use 10.0.2.2 for Android Emulator, localhost for iOS Simulator
   // For physical device, use your machine's LAN IP
-  static const String baseUrl = 'http://10.232.73.99:4000/api';
+  static const String baseUrl = 'http://10.0.2.2:4000/api';
   
   static const String authOtpRequest = '$baseUrl/auth/otp/request';
   static const String authOtpVerify = '$baseUrl/auth/otp/verify';
